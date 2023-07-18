4f616dc7c415bbe9c782d65124d431c7586e3383
Oliver Cvetkoski <cvetbt@gmail.com>
2023-07-18 11:19:12 +0200

    aaaaaa

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,26 @@
+import { EventEmitter, Injectable } from "@angular/core";
+
+import { Recipe } from "./recipes.model";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+
+
+@Injectable()
+export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
+    private recipes: Recipe[] = [
+        new Recipe('Test', 'this is a test', 'https://assets.bonappetit.com/photos/64349ba03fd52da4745a35f4/1:1/w_1920,c_limit/04102023-ratatouille-lede.jpg', [new Ingredient('Meat', 1)]),
+        new Recipe('Test 2', 'this is a test', 'https://assets.bonappetit.com/photos/64349ba03fd52da4745a35f4/1:1/w_1920,c_limit/04102023-ratatouille-lede.jpg', [new Ingredient('French fries', 10)])
+    ];
+
+    constructor(private slService: ShoppingListService) { }
+
+    getRecipes() {
+        return this.recipes.slice()
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngretiens(ingredients);
+    }
+
+}
